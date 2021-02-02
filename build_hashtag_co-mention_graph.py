99b2a7b357fd1a0bd9e14e3c3aa6df4271d45e53
Derek Weber <weber.dc@gmail.com>
2021-02-02 10:39:47 +1030

    Now reads gzipped inputs and deals with the Twitter election integrity CSV formatted datasets

--- build_hashtag_co-mention_graph.py
+++ build_hashtag_co-mention_graph.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import csv
+import gzip
 import json
 import networkx as nx
 import sys
@@ -7,6 +9,7 @@ import sys
 
 from argparse import ArgumentParser
 from utils import lowered_hashtags_from
+from raw_to_csv import parse_ira_hashtags
 
 
 # Builds a graphml file of a hashtag networks, connected when hashtags are mentioned
@@ -41,6 +44,13 @@ class Options:
             default=False,
             dest='strict',
             help='Only link hashtags occurring in the same tweet (default: False)'
+        )
+        self.parser.add_argument(
+            '--ira',
+            action='store_true',
+            default=False,
+            dest='ira',
+            help='Expect data in Twitter\'s election integrity format (CSV) (default: False)'
         )
         self.parser.add_argument(
             '-i',
@@ -87,6 +97,34 @@ def add_weighted_edge(g, u, v, delta=1, weight_property='weight'):
     g[u][v][weight_property] += float(delta)
 
 
+def process(user_id, hashtags, users_hashtags, cooccurring_hashtags):
+    if user_id not in users_hashtags:
+        users_hashtags[user_id] = dict([(ht, 1) for ht in hashtags])
+    else:
+        for ht in hashtags:
+            if ht not in users_hashtags[user_id]:
+                users_hashtags[user_id][ht] = 0
+            users_hashtags[user_id][ht] += 1
+    if len(hashtags) > 1:
+        hashtags.sort()
+        ht1_idx = 0
+        ht2_idx = 1
+        for ht1_idx in range(0, len(hashtags) - 1):
+            for ht2_idx in range(ht1_idx + 1, len(hashtags)):
+                key = (hashtags[ht1_idx], hashtags[ht2_idx])
+                if key not in cooccurring_hashtags:
+                    cooccurring_hashtags[key] = 0
+                cooccurring_hashtags[key] += 1
+
+
+
+def log_row(tweet_count):
+    tweet_count += 1
+    if DEBUG and tweet_count %  100 == 0: eprint('.', end='')
+    if DEBUG and tweet_count % 5000 == 0: eprint(' %10d' % tweet_count)
+    return tweet_count
+
+
 def eprint(*args, **kwargs):
     """Print to stderr"""
     print(*args, file=sys.stderr, **kwargs)
@@ -109,6 +147,7 @@ if __name__=='__main__':
     min_weight = opts.min_weight
     dry_run    = opts.dry_run
     strict     = opts.strict
+    ira        = opts.ira
 
     log('In JSON:     %s' % in_file)
     log('Out GraphML: %s' % out_file)
@@ -116,38 +155,41 @@ if __name__=='__main__':
     log('Min weight:  %d' % min_weight)
     log('Dry run:     %s' % dry_run)
     log('Strict:      %s' % strict)
+    log('IRA:         %s' % ira)
 
     # record the hashtag uses
     users_hashtags = {}  # user_id : map(hashtags:counts)
     cooccurring_hashtags = {}  # (ht1, ht2) : count, ht1 < ht2
     tweet_count = 0
-    with open(in_file, 'r', encoding='utf-8') as f:
-        for line in f.readlines():
-            tweet_count += 1
-            if DEBUG and tweet_count %  100 == 0: eprint('.', end='')
-            if DEBUG and tweet_count % 5000 == 0: eprint(' %10d' % tweet_count)
+
+    # in_f = gzip.open(in_file, 'rt', encoding='utf-8') if in_file[-1] in 'zZ' else open(in_file, 'r', encoding='utf-8')
+    if ira:
+        in_f = gzip.open(in_file, 'rt', encoding='utf-8') if in_file[-1] in 'zZ' else open(in_file, 'r', encoding='utf-8')
+    else:
+        in_f = gzip.open(in_file, 'rt') if in_file[-1] in 'zZ' else open(in_file, 'r', encoding='utf-8')
+    if not ira:
+        # with open(in_file, 'r', encoding='utf-8') as f:
+        for line in in_f: #f.readlines():
+            tweet_count = log_row(tweet_count)
 
             tweet = json.loads(line)
-
             user_id = tweet['user']['id_str']
             hashtags = [ht for ht in lowered_hashtags_from(tweet) if ht not in to_ignore]
-            if user_id not in users_hashtags:
-                users_hashtags[user_id] = dict([(ht, 1) for ht in hashtags])
-            else:
-                for ht in hashtags:
-                    if ht not in users_hashtags[user_id]:
-                        users_hashtags[user_id][ht] = 0
-                    users_hashtags[user_id][ht] += 1
-            if len(hashtags) > 1:
-                hashtags.sort()
-                ht1_idx = 0
-                ht2_idx = 1
-                for ht1_idx in range(0, len(hashtags) - 1):
-                    for ht2_idx in range(ht1_idx + 1, len(hashtags)):
-                        key = (hashtags[ht1_idx], hashtags[ht2_idx])
-                        if key not in cooccurring_hashtags:
-                            cooccurring_hashtags[key] = 0
-                        cooccurring_hashtags[key] += 1
+
+            process(user_id, hashtags, users_hashtags, cooccurring_hashtags)
+
+    else: # ira
+        csv_reader = csv.DictReader(in_f)
+        for tweet in csv_reader:
+            tweet_count = log_row(tweet_count)
+
+            user_id = tweet['userid']
+            hashtags = [ht for ht in parse_ira_hashtags(tweet['hashtags']) if ht not in to_ignore]
+
+            process(user_id, hashtags, users_hashtags, cooccurring_hashtags)
+
+    in_f.close()
+
 
     log('')
     log('Tweets: %d' % tweet_count)
